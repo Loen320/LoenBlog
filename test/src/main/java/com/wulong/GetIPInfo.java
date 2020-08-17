e60a974417bfcd9eb904fab288c3fa7e612ac435
loen <919433216@qq.com>
2020-08-17 19:27:40 +0800

    代码入库

---
+++ test/src/main/java/com/wulong/GetIPInfo.java
@@ -0,0 +1,10 @@
+package com.wulong;
+
+/**
+ * @version 5.0.0
+ * @Author: loen
+ * @date: 2020/8/12 12:42
+ * copyright @2019 Beijing Morong Information Techology CO.,Ltd.
+ */
+public class GetIPInfo {
+}
